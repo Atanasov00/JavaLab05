3f08c1d58ea3d82fd9a29030fc8fb1c76b49cb7c
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2020-11-11 22:23:03 +0100

    Completed the exercise in it.unibo.oop.lab05.ex1.UseSet

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,9 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link Set}.
@@ -34,5 +37,23 @@ public final class UseSet {
          * 
          * 6) Verifies if all the numbers left in the set are even
          */
+    	
+    	final Set<String> coll = new TreeSet<>();
+    	coll.addAll(Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"));
+    	
+    	System.out.println(coll);
+    	
+    	int i;
+    	Iterator<String> it = coll.iterator();
+    	while (it.hasNext()) {
+    		i = Integer.parseInt(it.next());
+    		if(i % 3 != 0) {
+    			it.remove();
+    		}
+    	}
+    	
+    	for(final String s: coll) {
+    		System.out.println(s);
+    	}
     }
 }
