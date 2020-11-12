dedf24610fae7f7e7c07be43676741e3cbf5b683
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2020-11-12 23:59:42 +0100

    Completed the exercise 5, implemented the functions in the class Utilities.java

--- src/it/unibo/oop/lab05/ex5/Utilities.java
+++ src/it/unibo/oop/lab05/ex5/Utilities.java
@@ -1,6 +1,7 @@
 package it.unibo.oop.lab05.ex5;
 
 import java.util.Collection;
+import java.util.Random;
 
 /**
  *
@@ -21,6 +22,7 @@ public final class Utilities {
      *            Collection type
      */
     public static <X> void copyAll(final Collection<X> source, final Collection<? super X> target) {
+    	target.addAll(source);
     }
 
     /**
@@ -31,8 +33,11 @@ public final class Utilities {
      * @return a random element from the collection
      * 
      */
-    public static <X> X getRandomElement(final Collection<X> coll) {
-        return null;
+	public static <X> X getRandomElement(final Collection<X> coll) {
+        X [] array = (X[]) new Object[coll.size()]; 
+        array = coll.toArray(array);
+        Random rd = new Random();
+    	return array[rd.nextInt(coll.size())];
     }
 
     /**
@@ -47,6 +52,15 @@ public final class Utilities {
      * @return a pair with two random elements
      */
     public static <X, Y> Pair<X, Y> getRandomPair(final Collection<X> one, final Collection<Y> two) {
-        return null;
+    	X[] array1 = (X[]) new Object[one.size()];
+    	Y[] array2 = (Y[]) new Object[two.size()];
+    	
+    	array1 = one.toArray(array1);
+    	array2 = two.toArray(array2);
+    	
+    	Random rd = new Random();
+    	
+    	Pair<X, Y> pair = new Pair<>(array1[rd.nextInt(one.size())], array2[rd.nextInt(two.size())]);
+    	return pair;
     }
 }
